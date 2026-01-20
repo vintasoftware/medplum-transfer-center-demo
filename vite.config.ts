681aa458b7dddcc46d5f14e3b881e86373b38cb5
bihellzin <oliva.gabriel@outlook.com>
2026-01-20 18:02:55 -0300

    update base to make github pages work

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/medplum-transfer-center-demo/',
   plugins: [react()],
   server: {
     host: 'localhost',
